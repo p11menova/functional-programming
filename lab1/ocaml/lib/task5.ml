80ece861771e18f1a86894272ee29cbbbfac010b
p11menova <p11menova@yandex.ru>
2025-10-27 22:23:37 +0300

    fixed ci + formatted

--- lab1/ocaml/lib/task5.ml
+++ lab1/ocaml/lib/task5.ml
@@ -1,18 +1,12 @@
-let rec gcd a b =
-   if b = 0 then a 
-  else gcd b (a mod b)
+let rec gcd a b = if b = 0 then a else gcd b (a mod b)
 
 let lcm a b = a / gcd a b * b
 
 (* хвостовая рекурсия *)
 let lcm_from_one_to_target_tail_rec n =
-  (* aux - вспомогательная внутр. функция ; 
-  aux (i - текущий элемент, acc - накопленное значение - НОК(1..i-1)) *)
-  let rec aux i acc = 
-    if i > n then acc
-     else aux (i + 1) (lcm acc i) in
+  (* aux - вспомогательная внутр. функция ; aux (i - текущий элемент, acc - накопленное значение -
+     НОК(1..i-1)) *)
+  let rec aux i acc = if i > n then acc else aux (i + 1) (lcm acc i) in
   aux 1 1
 
-
 let () = Printf.printf "LCM(1..10) = %d\n" (lcm_from_one_to_target_tail_rec 10)
-    ;;
