07926a2bec410fbb1c3254577c1059e6c5d7782b
p11menova <p11menova@yandex.ru>
2025-11-10 20:48:17 +0300

    task5: list generation + fold left, fold right impl

--- lab1/ocaml/lib/task5.ml
+++ lab1/ocaml/lib/task5.ml
@@ -1,14 +1,43 @@
-let rec gcd a b = if b = 0 then a else gcd b (a mod b)
+let rec gcd a b =
+  match b with
+  | 0 -> a
+  | _ -> gcd b (a mod b)
 
 let lcm a b = a / gcd a b * b
 
 (* хвостовая рекурсия *)
 let lcm_from_one_to_target_tail_rec n =
-  (* aux - вспомогательная внутр. функция ; aux (i - текущий элемент, acc - накопленное значение -
-     НОК(1..i-1)) *)
-  let rec aux i acc = if i > n then acc else aux (i + 1) (lcm acc i) in
+  (* aux - вспомогательная внутр. функция ; aux (i - текущий элемент, acc - накопленное значение - НОК(1..i-1)) *)
+  let rec aux i acc =
+    match i with
+    | x when x > n -> acc
+    | _ -> aux (i + 1) (lcm acc i)
+  in
   aux 1 1
 
 (* рекурсия *)
-let rec lcm_from_one_to_target_rec n =
-  if n = 1 then 1 else lcm n (lcm_from_one_to_target_rec (n - 1))
+let rec lcm_from_one_to_target_rec = function
+  | 1 -> 1
+  | n -> lcm n (lcm_from_one_to_target_rec (n - 1))
+
+(* let range n = Seq.(1 -- n |> List.of_seq) я хотела так но у меня не поддерживается оператор -- *)
+
+(* генерация последовательности *)
+(* range n is_reversed *)
+let range n = function
+  | true -> List.init n (fun i -> n - i) (* List.init length fun = [fun 0; fun 1; ... ; fun length]*)
+  | false -> List.init n succ
+
+(* свертка *)
+(* List.fold_right: [a; b; c] -> f a (f b (f c init))
+
+   List.fold_left: [a; b; c] -> f (f (f init a) b) c
+
+   [3; 2; 1] -> lcm 3 (lcm 2 (lcm 1 1)) -> lcm 3 (lcm 2 1) -> lcm 3 2 -> 6 => fold_right fits
+
+   [1; 2; 3] -> lcm (lcm ( lcm 1 1) 2) 3 -> lcm ( lcm 1 2) 3 -> lcm 2 3 -> 6 => fold_left fits 
+*)
+
+let lcm_from_one_to_target_fold_left n = List.fold_left lcm 1 (range n false)
+
+let lcm_from_one_to_target_fold_right n = List.fold_right lcm (range n true) 1
--- lab1/ocaml/test/test_task5.ml
+++ lab1/ocaml/test/test_task5.ml
@@ -1,5 +1,4 @@
 open Alcotest
-
 let test_gcd () =
   check int "gcd(54,24)=6" 6 (Task5.gcd 54 24) ;
   check int "gcd(5,0)=5" 5 (Task5.gcd 5 0) ;
@@ -11,6 +10,7 @@ let test_lcm () =
   check int "lcm(3,4)=12" 12 (Task5.lcm 3 4) ;
   check int "lcm(12,15)=60" 60 (Task5.lcm 12 15)
 
+
 let test_lcm_range_tail_rec () =
   check int "LCM(1..10)=2520" 2520 (Task5.lcm_from_one_to_target_tail_rec 10) ;
   check int "LCM(1..1)=1" 1 (Task5.lcm_from_one_to_target_tail_rec 1) ;
@@ -21,10 +21,24 @@ let test_lcm_range_rec () =
   check int "LCM(1..2)=2" 2 (Task5.lcm_from_one_to_target_rec 2) ;
   check int "LCM(1..20)=232792560" 232792560 (Task5.lcm_from_one_to_target_rec 20)
 
+
+let test_lcm_range_fold () =
+  check int "LCM(1..10)=2520" 2520 (Task5.lcm_from_one_to_target_fold_left 10) ;
+  check int "LCM(1..2)=2" 2 (Task5.lcm_from_one_to_target_fold_left 2) ;
+  check int "LCM(1..20)=232792560" 232792560 (Task5.lcm_from_one_to_target_fold_left 20) ;
+  check int "LCM(1..1)=1" 1 (Task5.lcm_from_one_to_target_fold_left 1);
+  check int "LCM(1..10)=2520" 2520 (Task5.lcm_from_one_to_target_fold_right 10) ;
+  check int "LCM(1..2)=2" 2 (Task5.lcm_from_one_to_target_fold_right 2) ;
+  check int "LCM(1..20)=232792560" 232792560 (Task5.lcm_from_one_to_target_fold_right 20) ;
+  check int "LCM(1..1)=1" 1 (Task5.lcm_from_one_to_target_fold_right 1)
+
 let () =
   let open Alcotest in
   run "task5"
     [ ("gcd", [test_case "basic gcd" `Quick test_gcd])
     ; ("lcm", [test_case "basic lcm" `Quick test_lcm])
     ; ("lcm_range_tail_rec", [test_case "lcm(1..n) - tail recursion" `Quick test_lcm_range_tail_rec])
-    ; ("lcm_range_rec", [test_case "lcm(1..n) - recursion" `Quick test_lcm_range_rec]) ]
+    ; ("lcm_range_rec", [test_case "lcm(1..n) - recursion" `Quick test_lcm_range_rec]) 
+    ; ("lcm_range_fold", [test_case "lcm(1..n) - list generation + fold" `Quick test_lcm_range_fold])]
+
+
